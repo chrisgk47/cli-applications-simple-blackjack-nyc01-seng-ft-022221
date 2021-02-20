13044f9abbc15e34517648c6c22b51778c1e046a
Christopher Kim <chris.kim1990@gmail.com>
2021-02-20 03:37:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,10 +65,12 @@ end
 def runner
   # code runner here
   welcome
-  initial_round
-  hit?
-  display_card_total
+  card_total = initial_round
+  
   until card_total > 21
-    end_game
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
+  end_game(card_total)
 end
+
